9c9d09809d17c26642eb415ba1260b6e9d3e012d
macoba69 <carlos.matelunab@gmail.com>
2018-10-15 14:18:07 -0300

    Add project page

--- server.js
+++ server.js
@@ -39,8 +39,16 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
+        
+    });
+});
+
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+        
     });
 });
 
